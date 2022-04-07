c413551e8d06068b3a46f08d558cfdcb238f6900
Silver1793 <git>
2022-04-06 21:54:23 -0400

    small changes

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -8,6 +8,7 @@ function Hangman() {
   const [word, setWord] = useState(""); //useState(chooseRandom());
   const [over, setOver] = useState(false);
   const [guessed, setGuessed] = useState([]);
+  const [input, setInput] = useState("");
   const baseUrl = process.env.baseURL || "http://localhost:6001";
   const other = "https://richardthisisatest.herokuapp.com";
 
@@ -36,6 +37,9 @@ function Hangman() {
     setGuessed([]);
     setMistake(0);
   };
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
 
   const handleButtonClick = (e) => {
     if (word.includes(e.target.value) === false) {
@@ -57,6 +61,15 @@ function Hangman() {
         {letter}
       </button>
     ));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (word.includes(input) === false) {
+      setMistake((prev) => prev + 1);
+    }
+    setGuessed((old) => [...old, input]); //guessed.add(e.target.value));
+    console.log("HERE");
+    setInput("");
   };
 
   const genButtonsSec = () => {
@@ -90,6 +103,17 @@ function Hangman() {
           Restart
         </button>
       </div>
+      <form onSubmit={handleSubmit} className="centered">
+        <input
+          placeholder="Enter a Letter here"
+          maxLength={1}
+          onChange={handleChange}
+          value={input}
+          type="text"
+          name="name"
+        />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
